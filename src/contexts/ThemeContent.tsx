cc8c192ad9edabc5fb5f6946e0c8dd50a3b9616f
Yosef <mercytv932@gmail.com>
2026-09-11 00:05:15 -0400

    ToggleTheme

---
+++ src/contexts/ThemeContent.tsx
@@ -0,0 +1,26 @@
+import { createContext, useState, type ReactNode } from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  function toggleTheme() {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export { ThemeContext, ThemeProvider };
