7cb513f9f4982e8a8dd61be0dd83823b54ca2ecb
Yosef <mercytv932@gmail.com>
2026-09-10 21:15:35 -0400

    addTodo function

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -3,11 +3,21 @@ import { createContext, useState } from "react";
 import type { Todo } from "../types";
 export interface TodoContextType {
   todos: Todo[];
+  addTodo: (text: string) => void;
 }
 const TodoContext = createContext();
 
 function TodoProvider({ children }: { children: ReactNode }) {
   const [todos, setTodos] = useState<Todo[]>([]);
+
+  function addTodo(text: string) {
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: text,
+      completed: false,
+    };
+    setTodos((prev) => [...prev, newTodo]);
+  }
 
   return <div></div>;
 }
