12c079447464eaef6d22c0118d0c561576f4b453
Yosef <mercytv932@gmail.com>
2026-09-11 00:22:48 -0400

    done with App importing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-
-
-
+import { TodoProvider } from "./contexts/TodoContext";
+import { FilterProvider } from "./contexts/FilterContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <TodoProvider>
+        <FilterProvider>
+          <ThemeProvider></ThemeProvider>
+        </FilterProvider>
+      </TodoProvider>
+    </div>
+  );
 }
+
+export default App;
