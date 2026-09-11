2431eb689cd00d1dc0d98340bd77c5c1a3be7e2f
Yosef <mercytv932@gmail.com>
2026-09-11 01:13:40 -0400

    app functionality all done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { TodoProvider } from "./contexts/TodoContext";
 import { FilterProvider } from "./contexts/FilterContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import ThemeToggleButton from "./components/ThemeToggleButton";
+import TodoInput from "./components/TodoInput";
+import FilterButtons from "./components/FilterButtons";
+import TodoList from "./components/TodoList";
 function App() {
   return (
     <div>
@@ -8,6 +12,10 @@ function App() {
         <FilterProvider>
           <ThemeProvider>
             <h1>Todo App</h1>
+            <ThemeToggleButton />
+            <TodoInput />
+            <FilterButtons />
+            <TodoList />
           </ThemeProvider>
         </FilterProvider>
       </TodoProvider>
