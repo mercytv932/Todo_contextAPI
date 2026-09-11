1291976ae996ae70612aca1ca5f397889ce763c7
Yosef <mercytv932@gmail.com>
2026-09-11 09:22:36 -0400

    done with clearCompleted

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { FilterContext } from "../contexts/FilterContext";
+import { TodoContext } from "../contexts/TodoContext";
 
 function FilterButtons() {
   const context = useContext(FilterContext);
@@ -10,11 +11,20 @@ function FilterButtons() {
 
   const { filter, setFilter } = context;
 
+  const todoContext = useContext(TodoContext);
+
+  if (!todoContext) {
+    throw new Error("FilterButtons must be used inside TodoProvider");
+  }
+
+  const { clearCompleted } = todoContext;
+
   return (
     <div>
       <button onClick={() => setFilter("all")}>All</button>
       <button onClick={() => setFilter("active")}>Active</button>
       <button onClick={() => setFilter("completed")}>Completed</button>
+      <button onClick={clearCompleted}>Clear Completed</button>
     </div>
   );
 }
