4ca9ae8507c776921fd29a244bdfaf39cca97d36
Yosef <mercytv932@gmail.com>
2026-09-11 01:27:53 -0400

    TodoItemdone Editing functionality done

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,7 +9,7 @@ function TodoItem({ todo }: { todo: Todo }) {
   const [editText, setEditText] = useState(todo.text);
 
   if (!context) {
-    throw new Error("TodoItem must  be used inside TodoProvider");
+    throw new Error("TodoItem must be used inside TodoProvider");
   }
 
   const { toggleTodo, deleteTodo, editTodo } = context;
@@ -27,12 +27,28 @@ function TodoItem({ todo }: { todo: Todo }) {
 
   return (
     <div>
-      <span>{todo.text}</span>
-      <button onClick={() => toggleTodo(todo.id)}>
-        {todo.completed ? "undo" : "complete"}
-      </button>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+          />
 
-      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          <button onClick={handleEdit}>Save</button>
+          <button onClick={() => setIsEditing(false)}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <span>{todo.text}</span>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+          <button onClick={() => toggleTodo(todo.id)}>
+            {todo.completed ? "undo" : "complete"}
+          </button>
+
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </>
+      )}
     </div>
   );
 }
