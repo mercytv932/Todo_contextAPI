0e6de3b3418ce179b91264ce5e26c283b023af14
Yosef <mercytv932@gmail.com>
2026-09-11 01:02:44 -0400

    done with todolist

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { TodoContext } from "../contexts/TodoContext";
 import TodoItem from "./TodoItem";
+import { FilterContext } from "../contexts/FilterContext";
 
 function TodoList() {
   const context = useContext(TodoContext);
@@ -11,9 +12,27 @@ function TodoList() {
 
   const { todos } = context;
 
+  const filterContext = useContext(FilterContext);
+  if (!filterContext) {
+    throw new Error("TodoList must be used inside FilterProvider");
+  }
+
+  const { filter } = filterContext;
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === "active") {
+      return !todo.completed;
+    }
+
+    if (filter === "completed") {
+      return todo.completed;
+    }
+    return true;
+  });
+
   return (
     <div>
-      {todos.map((todo) => (
+      {filteredTodos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
     </div>
