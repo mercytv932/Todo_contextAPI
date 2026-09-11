487461f94cba766c6fc3a865cb378e891665d719
Yosef <mercytv932@gmail.com>
2026-09-11 00:53:06 -0400

    TodoItem done

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { TodoContext } from "../contexts/TodoContext";
+import type { Todo } from "../types";
+
+function TodoItem({ todo }: { todo: Todo }) {
+  const context = useContext(TodoContext);
+
+  if (!context) {
+    throw new Error("TodoItem mustr be used inside TodoProvider");
+  }
+
+  const { toggleTodo, deleteTodo, editTodo } = context;
+
+  return (
+    <div>
+      <span>{todo.text}</span>
+      <button onClick={() => toggleTodo(todo.id)}>
+        {todo.completed ? "undo" : "complete"}
+      </button>
+
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default TodoItem;
