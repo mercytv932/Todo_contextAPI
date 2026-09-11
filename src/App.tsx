79d7747fdea0856b73ff7244fa158af3ab057f5f
Yosef <mercytv932@gmail.com>
2026-09-11 09:38:17 -0400

    Toggle Theme functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,37 @@ import ThemeToggleButton from "./components/ThemeToggleButton";
 import TodoInput from "./components/TodoInput";
 import FilterButtons from "./components/FilterButtons";
 import TodoList from "./components/TodoList";
+import { useContext } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
+import "./App.css";
+
+function TodoApp() {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error("App must be used inside ThemeProvider");
+  }
+
+  const { theme } = context;
+
+  return (
+    <div className={theme}>
+      <h1>Todo App</h1>
+      <ThemeToggleButton />
+      <TodoInput />
+      <FilterButtons />
+      <TodoList />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <TodoProvider>
         <FilterProvider>
           <ThemeProvider>
-            <h1>Todo App</h1>
-            <ThemeToggleButton />
-            <TodoInput />
-            <FilterButtons />
-            <TodoList />
+            <TodoApp />
           </ThemeProvider>
         </FilterProvider>
       </TodoProvider>
